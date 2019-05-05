17cb337a849b999dee3d57d301e7f1aecb9b0592
André Max <Andr Max@DESKTOP-TGT5UPI>
2019-05-05 00:34:28 -0300

    files ara now sync

--- src/connectfour/ArtificialInteligence.java
+++ src/connectfour/ArtificialInteligence.java
@@ -4,6 +4,101 @@ package connectfour;
  *
  * @author André Max
  */
+
 public class ArtificialInteligence {
-    int minimax()
+    private static final int[][] EVALUATIONTABLE = {{3, 4, 5, 7, 5, 4, 3}, 
+                                          {4, 6, 8, 10, 8, 6, 4},
+                                          {5, 8, 11, 13, 11, 8, 5}, 
+                                          {5, 8, 11, 13, 11, 8, 5},
+                                          {4, 6, 8, 10, 8, 6, 4},
+                                          {3, 4, 5, 7, 5, 4, 3}};
+    
+    public static int evaluateContent(Board gBoard) {
+        int utility = 138;
+        int sum = 0;
+        for (int i = 0; i < 6; i++)
+            for (int j = 0; j <7; j++)
+                if (gBoard.gameBoard[i][j] == gBoard.PLAYER1)
+                    sum += EVALUATIONTABLE[i][j];
+                else if (gBoard.gameBoard[i][j] == gBoard.PLAYER2)
+                    sum -= EVALUATIONTABLE[i][j];
+        return utility + sum;
+    }
+    
+    public static boolean checkWin(char badge, Board board) {
+        return checkHorizontalStreaks(board.gameBoard, badge)
+            || checkVerticalStreaks(board.gameBoard, badge);
+    }
+
+    private static boolean checkHorizontalStreaks(char[][] board, char badge) {
+        for (int row = 0; row < board.length; row++) {
+            // loop throught each row
+            int currentStreak = 0;
+            for (int col = 0; col < board[row].length; col++) {
+                // loop through each column in the row
+                if (board[row][col] == badge) {
+                    // keep the streak of 'badge' going
+                    currentStreak++;
+                    if (currentStreak == 4) {
+                        // winner
+                        return true;
+                    }
+                } else {
+                    // restart the streak
+                    currentStreak = 0;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private static boolean checkVerticalStreaks(char[][] board, char badge) {
+        for (int row = 0; row < 7; row++) {
+            // loop throught each row
+            int currentStreak = 0;
+            for (int col = 0; col < 6; col++) {
+                // loop through each column in the row
+                if (board[col][row] == badge) {
+                    // keep the streak of 'badge' going
+                    currentStreak++;
+                    if (currentStreak == 4) {
+                        // winner
+                        return true;
+                    }
+                } else {
+                    // restart the streak
+                    currentStreak = 0;
+                }
+            }
+        }
+        return false;
+    }
+    
+    static int minimax(Board gBoard, int depth, int alpha, int beta, boolean maximazingPlayer){
+        if (checkWin(maximazingPlayer? gBoard.PLAYER2:gBoard.PLAYER1, gBoard) || depth == 0){
+            return evaluateContent(gBoard);
+        }
+        if (maximazingPlayer){
+            for (int i = 0; i < 7; i++){
+                Board board = new Board(gBoard.gameBoard);
+                board.addPiece(i, 2);
+                alpha = Math.max(alpha, minimax(board, depth - 1, alpha, beta, false));
+                if (beta <= alpha){
+                    return alpha;
+                }
+            }
+            return alpha;
+        }
+        else{
+            for (int i = 0; i < 7; i++){
+                Board board = new Board(gBoard.gameBoard);
+                board.addPiece(i, 1);
+                beta = Math.min(beta, minimax(board, depth - 1, alpha, beta, true));
+                if (beta <= alpha){
+                    return beta;
+                }     
+            }
+            return beta;
+        }
+    }
 }
--- src/connectfour/Board.java
+++ src/connectfour/Board.java
@@ -6,15 +6,61 @@ package connectfour;
  */
 public class Board {
     char[][] gameBoard;
+    final char VAZIO = 'X';
+    final char PLAYER1 = '1';
+    final char PLAYER2 = '2';
+    int last = 0;
+    
+    Board(char[][] gameBoard){
+        this.gameBoard = new char[6][7];
+        for (int i = 0; i < 6; i++){
+            for (int j = 0; j < 7; j++){
+                this.gameBoard[i][j] = gameBoard[i][j];
+            }
+        }
+    }
     
     Board(){
         gameBoard = new char[6][7];
         for (int i = 0; i < 6; i++){
             for (int j = 0; j < 7; j++){
-                gameBoard[i][j] = ' ';
+                gameBoard[i][j] = VAZIO;
             }
         }
     }
     
+    public char chooseSymbol(int player){
+        return player  == 1 ? '1' : '2'; 
+    }
     
+    public boolean addable(char[][] gameBoard, int col){
+        int i = 0;
+        while (i < 6 && gameBoard[i][col] == VAZIO ){
+            i++;
+        }
+        this.last = i;
+        if (i != 0){
+            return true;
+        }
+        return false;
+        
+    }
+    
+    public void addPiece(int col, int player){
+        if (addable(this.gameBoard, col)){
+            this.gameBoard[last-1][col] = chooseSymbol(player);
+        }
+        
+        
+    }
+    
+    public void showBoard(){
+        for (int i = 0; i < 6; i++){
+            for (int j = 0; j < 7; j++){
+                System.out.printf("%c ", gameBoard[i][j]);  
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
 }

6d953f523702494e7bf0721fe50e2cdb748c8c3e
AndreMax7303 <45076397+AndreMax7303@users.noreply.github.com>
2019-05-04 22:37:03 -0300

    Add files via upload

--- src/connectfour/ConnectFour.java
+++ src/connectfour/ConnectFour.java
@@ -1,4 +1,5 @@
 package connectfour;
+import java.util.Scanner;
 
 /**
  *
@@ -9,8 +10,43 @@ public class ConnectFour {
     /**
      * @param args the command line arguments
      */
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Board board = new Board();
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            int[] scores = new int[7];
+            System.out.println("Digite a coluna: ");
+            int playerInput = scanner.nextInt();
+            board.addPiece(playerInput, 1);
+            
+            
+            for (int i = 0 ; i < 7; i++){
+                int j = 0;
+                while (j < 6 && board.gameBoard[j][i] == board.VAZIO){
+                    j++;
+                }
+                scores[i] = ArtificialInteligence.minimax(board, 1, -3000, 3000, true);
+                System.out.println(scores[i]);
+                
+            }
+            System.out.println(getMax(scores));
+            board.addPiece(getMax(scores), 2);
+            board.showBoard();
+        }
+    
+    }
+    public static int getMax(int[] numbers) {
+        int maxValue = numbers[0];
+        int pos = 0;
+           for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] > maxValue) {
+                    maxValue = numbers[i];
+                    pos = i;
+                }
+            }
+        return pos;
     }
     
 }
